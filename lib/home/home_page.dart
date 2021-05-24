a70c514e525e9faa075d220cc6adfd5ad6a177fe
Yara Sá <yarasasantana@gmail.com>
2021-05-24 09:06:41 -0300

    Alteraçao do botao de adicionar nota na pagina home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -17,11 +17,40 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: NoteCardWidget(
-          notes: notes,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              NoteCardWidget(
+                notes: notes,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      child: ElevatedButton.icon(
+                          onPressed: () async {
+                            var description = await Navigator.pushNamed(
+                                context, '/create-note');
+                            setState(() {
+                              notes.add(description! as String);
+                            });
+                          },
+                          icon: Icon(Icons.add),
+                          label: Text('Nota')),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      /*floatingActionButton: FloatingActionButton(
         onPressed: () async {
           var description = await Navigator.pushNamed(context, '/create-note');
           setState(() {
@@ -29,7 +58,7 @@ class _HomePageState extends State<HomePage> {
           });
         },
         child: Icon(Icons.add),
-      ),
+      ),*/
     );
   }
 }
